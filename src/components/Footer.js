b8af27a7668fb3d17f474a76e7b9c9c04294d507
anayag33 <anayagoradia2012@gmail.com>
2024-03-01 15:03:36 -0800

    navbar and footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='socialMedia'>
-        <InstagramIcon />
-        <TwitterIcon />
         <FacebookIcon />
-        <LinkedInIcon />
-        <p>&copy; 2024 daiso.com</p>
+        <InstagramIcon />
+        <YouTubeIcon />
+        <TwitterIcon />
+        <PinterestIcon />
       </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../styles/Navbar.css';
+import Logo from '../assets/DaisoLogo.png';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <p className='location'>Santa Clara Location Thingy</p>
+    <div>
+      <div className='navbarP'>
+        <div className='arrow'>
+          <ArrowDropDownIcon />
+        </div>
+        <p className='location'> Choose Location</p>
+      </div>
+      <div className='navbarW'>
+        <img className='logo' src={Logo}/>
+        <p className='location'>Home</p>
+        <p className='location'>About</p>
+        <input className='searchbar' type="text" placeholder="Search" />
+      </div>
     </div>
   )
 }
