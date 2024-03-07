c3062690c97889677dff2b23d632c49aa1b47e16
anayag33 <anayagoradia2012@gmail.com>
2024-03-06 17:46:15 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,37 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <div className='footer'>
+      <div className='horizontal'>
+        <div className='largeText'>
+          <h4>Quick Links</h4>
+          <div className='normalText'>
+            <a href="https://daiso.rec.pro.ukg.net/DAI1003DAIS/JobBoard/42da7f44-22fb-4245-8e53-e4c36c4e2133/?q=&o=postedDateDesc&w=&wc=&we=&wpst=" className='link'>Careers</a>
+            <a href="https://daisous.com/blogs/daiso-updates" className='link'>Daiso News</a>
+            <a href="https://daisous.com/pages/faq" className='link'>FAQ</a>
+            <a href="/storelocator" className='link'>Store Locator</a>
+            <a href="https://daisous.com/collections/ingredients" className='link'>Safety Datasheet</a>
+
+          </div>
+        </div>
+        <div className='largeText'>
+          <h4>Info</h4>
+          <div className='normalText'>
+            <a href="https://daisous.com/pages/website-and-technology-accessibility" className='link'>Accessibility</a>
+            <a href="https://daisous.com/pages/cp65" className='link'>California Proposition 65</a>
+            <a href="https://daisous.com/pages/privacy-policy" className='link'>Privacy Policy</a>
+            <a href="https://daisous.com/pages/terms-and-conditions" className='link'>Terms & Conditions</a>
+          </div>
+        </div>
+        <div className='largeText'>
+          <h4>Customer Service</h4>
+          <div className='normalText'>
+            <a href="https://daisous.com/pages/contact" className='link'>Contact Us</a>
+            <a href="https://daisous.com/policies/terms-of-service" className='link'>Terms of Service</a>
+            <a href="https://daisous.com/pages/return-policy" className='link'>Return Policy</a>
+            
+          </div>
+        </div>
+      </div>
       <div className='socialMedia'>
         <FacebookIcon />
         <InstagramIcon />
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/ItemCard.css';
 import { Link } from "react-router-dom";
 import Rating from '../assets/Rating.png'
+import { Favorite } from '@material-ui/icons';
 
 
 function ItemCard({image, name,  price, inStock}) {
+
+  const [fav, setFav] = useState(false);
+
+  function clickHeart(){
+    if (fav == false){
+      setFav(true);
+    }
+    if (fav == true){
+      setFav(false);
+    }
+  }
+
   return (
     <div className='itemCard'>
+      { fav ? (
+        <button className='heartButton' onClick={clickHeart}>
+          <Favorite/>
+        </button>
+      ) : (
+        <button className='emptyHeartButton' onClick={clickHeart}>
+          <Favorite/>
+        </button>
+      )}
       <div  className='image' style={{ backgroundImage: `url(${image})` }}></div>
       {/* <Link>
         <img  className='image' src={BannerImage} ></img>
       </Link> */}
-        <h3 className='text'>{name}</h3>
+        <h4 className='text'>{name}</h4>
         <img className='rating' src={Rating}></img>
         <p className='text'>${price}</p>
         {inStock ? (
