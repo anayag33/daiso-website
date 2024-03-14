30aea24fbbc5056ea8c7acec7ad569c02a0407fd
madinguyen9 <mnguyen9@scu.edu>
2024-03-14 16:09:35 -0700

    Finished Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ItemCard from '../components/ItemCard';
 import JumbotronImage from '../assets/home/Daiso-jumbotron.jpg';
 import IntroImage from '../assets/home/Daiso-Intro.png';
 import '../styles/Home.css';
@@ -8,7 +7,12 @@ import Plushie from '../assets/home/plushie-home.png'
 import Stationary from '../assets/home/stationary-home.png'
 import { Link } from 'react-router-dom';
 import Aisles from '../assets/home/aisles.png'
-
+import ItemCard from '../components/ItemCard';
+import ChocolatePocky from '../assets/ChocolatePocky.png';
+import RedAlarmClock from '../assets/RedAlarmClock.png';
+import GreenDinoPlushie from '../assets/GreenDinosaurPlushie.png';
+import ColoredMarkers from '../assets/ColoredMarkers.png';
+import BlueBearPlushie from '../assets/BlueBearPlushie.png';
 
 
 function Home() {
@@ -35,8 +39,37 @@ function Home() {
 
       <div className="newsletter-container">
       <img src={Aisles} alt="intro" className="aisles" />
+      <div className="newsletter-rectangle" >
+      <p>Register to get exclusive offers and the latest on our products!</p>
+            <br></br>
+            <h1>Subscribe For Latest Newsletter</h1>
+
+            <div className="subscribe-form">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
+        </div>
+            </div>
       </div>
+
+
+
+      <div className="category-container">
+        <h2>BESTSELLER PRODUCTS</h2>
+        <div className="pink-line"></div>
+        <div className="bestseller-rectangles">
+          <Link to='/pockyitem'>
+                <ItemCard image={ChocolatePocky} name={"Chocolate Pocky"} price={5} inStock={true}/>
+            </Link>
+            <ItemCard image={RedAlarmClock} name={"Red Alarm Clock"} price={1} inStock={true}/>
+            <ItemCard image={GreenDinoPlushie} name={"Green Dinosaur Plushie"} price={3} inStock={true}/>
+            <ItemCard image={ColoredMarkers} name={"5 Colored Markers"} price={7} inStock={true}/>
+            <Link to='/bluebearplushieitem'>
+                <ItemCard image={BlueBearPlushie} name={"Blue Bear Plushie"} price={3} inStock={false}/>
+          </Link>
+        </div>
     </div>
+    </div>
+    
   );
 }
 
