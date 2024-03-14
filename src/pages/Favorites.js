8f27b8b94617e031b490b6a42e2c24a69bf251fb
anayag33 <anayagoradia2012@gmail.com>
2024-03-14 11:32:13 -0700

    sarahs stuff

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import ChocolatePocky from '../assets/ChocolatePocky.png';
+import BlueBearPlushie from '../assets/BlueBearPlushie.png';
+import GreyOvenMitts from '../assets/GreyOvenMitts.jpg';
+import RamuneSodaOrange from '../assets/RamuneSodaOrange.png';
+import BlueBowls from '../assets/BlueBowls.png';
+import RedAlarmClock from '../assets/RedAlarmClock.png';
+import GreenDinoPlushie from '../assets/GreenDinosaurPlushie.png';
+import ColoredMarkers from '../assets/ColoredMarkers.png';
+import ItemCard from '../components/ItemCard';
+import { Link } from "react-router-dom";
+import '../styles/Favorites.css'
+
+
 
 function Favorites() {
   return (
     <div>
-      
+      <h1 className='heading'>Favorites (8)</h1>
+      <div className='containerBottom'>
+            <Link to='/pockyitem'>
+                <ItemCard image={ChocolatePocky} name={"Chocolate Pocky"} price={5} inStock={true}/>
+            </Link>
+            <Link to='/bluebearplushieitem'>
+                <ItemCard image={BlueBearPlushie} name={"Blue Bear Plushie"} price={3} inStock={false}/>
+            </Link>
+            <ItemCard image={GreyOvenMitts} name={"Grey Oven Mitts"} price={5} inStock={true}/>
+            <Link to='/ramuneitem'>
+                <ItemCard image={RamuneSodaOrange} name={"Ramune Orange Soda"} price={7} inStock={false}/>
+            </Link>
+        </div>
+        <div className='containerBottom'>
+            <ItemCard image={BlueBowls} name={"Set of 4 Blue Bowls"} price={1} inStock={true}/>
+            <ItemCard image={RedAlarmClock} name={"Red Alarm Clock"} price={1} inStock={true}/>
+            <ItemCard image={GreenDinoPlushie} name={"Green Dinosaur Plushie"} price={3} inStock={true}/>
+            <ItemCard image={ColoredMarkers} name={"5 Colored Markers"} price={7} inStock={true}/>
+        </div>
     </div>
   )
 }
--- src/pages/StoreLocator.js
+++ src/pages/StoreLocator.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from 'react';
+import '../styles/StoreLocator.css';
+import Map from '../assets/Map.jpeg';
+import { Link } from "react-router-dom";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 
 function StoreLocator() {
+
+  function clearResults() {
+    // This function clears the search results
+    document.querySelectorAll('.store').forEach(function(store) {
+        store.style.display = 'none';
+    });
+}
   return (
     <div>
-      
+      <div className='banner'>
+            <p className='bannerText'>
+                <div className='mainCard'>
+                    <Link to='/'>
+                        <button className='bannerButton'>Home</button> 
+                    </Link>
+                    <ArrowForwardIosIcon />
+                    <Link to='/pockyitem'>
+                        <button className='bannerButton'>Store Locator</button> 
+                    </Link>
+                </div>
+            </p>
+        </div>
+  <div class="store-locator-container">
+    <div class="search-container">
+        <h1 className='heading'>Find My Store</h1>
+        <div>
+            <input className="form" type="text" id="zipcode" placeholder="Zipcode" />
+            <input className="form" type="text" id="city" placeholder="City" />
+            <input className="form" type="text" id="state" placeholder="State" />
+        </div>
+        <button className='pinkButton'>Find Store</button>
+        <button className='greyButton'>Clear</button>
+        <div className='containerBottom'>
+          <div>
+          <div className='storeCard'>
+                <p className='storeLocation'>San Jose</p>
+                <p>588 Saratoga Ave Unit 20</p>
+                <p>San Jose, CA 95129</p>
+                <p>2 miles away from you</p>
+            </div>
+            <div className='storeCard'>
+              <p className='storeLocation'>Cupertino</p>
+                <p>19750 Stevens Creek Blvd</p>
+                <p>Cupertino, CA 95014</p>
+                <p>5 miles away from you</p>
+            </div>
+            <div className='storeCard'>
+                <p className='storeLocation'>San Jose</p>
+                <p>588 Saratoga Ave Unit 20</p>
+                <p>San Jose, CA 95129</p>
+                <p>2 miles away from you</p>
+            </div>
+            <div className='storeCard'>
+            <p className='storeLocation'>Cupertino</p>
+                <p>19750 Stevens Creek Blvd</p>
+                <p>Cupertino, CA 95014</p>
+                <p>10 miles away from you</p>
+            </div>
+          </div>
+          <img src={Map} alt="Map Placeholder" className='map'/> 
+        </div>
+    </div>
+</div>
     </div>
   )
 }
